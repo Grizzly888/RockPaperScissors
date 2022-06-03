b276245b4604e582230f9eda6d6f3992fd99cbe9
Grizzly888 <ebv1402@gmail.com>
2022-06-04 01:02:40 +0300

    updates

---
+++ js/simple.js
@@ -0,0 +1,78 @@
+let userScore = 0;
+let computerScore = 0;
+let gameScore=0;
+let userChoice;
+
+
+
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    computerSelection = choices[randomNumber];
+    return computerSelection;
+  }
+
+function playRound(playerSelection, computerSelection){
+    computerSelection = computerPlay();
+    console.log('Game function: user choice is = ' + playerSelection);
+    console.log('Game function: computer choice is = ' + computerSelection);
+
+    switch (playerSelection + computerSelection) {
+        case "paperrock":
+        case "rockscissors":
+        case "scissorspaper":
+          
+          gameScore = gameScore +1;
+          userScore = userScore +1;
+
+
+          //Console output
+          console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+          console.log(`User score is ${userScore}.`);
+          console.log(`Computer score is ${computerScore}.`);
+
+          break;
+        case "rockpaper":
+        case "scissorsrock":
+        case "paperscissors":
+          
+          gameScore = gameScore +1; 
+          computerScore= computerScore +1;
+
+
+          //Console output
+          console.log(`You Lost! ${computerSelection} beats ${playerSelection}`);
+          console.log(`User score is ${userScore}.`);
+          console.log(`Computer score is ${computerScore}.`);
+          break;
+        case "rockrock":
+        case "scissorsscissors":
+        case "paperpaper":
+          gameScore = gameScore +1;
+        
+          
+          //Console output
+          console.log("draw");
+          break;
+      }
+}
+
+function game(){
+   for (var i = 0; i <5; i++){
+       playRound(getUserChoice());
+   }
+   if(userScore > computerScore){
+    console.log("You Won a game of 5 rounds!")
+  }
+  else {
+    console.log("You Lost a game of 5 rounds!")
+  }
+}
+
+function getUserChoice(){
+    let userChoice = prompt("Enter Rock, Paper or Scissors").toLowerCase();
+    return userChoice;
+}
+
+        
+game();     
